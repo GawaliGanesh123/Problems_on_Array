947014168c0490c1d946afd0b317cd8cc8489ab8
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-03-04 12:16:42 +0530

    Add files via upload

---
+++ 29_2_Print_Alternate_Elements.c
@@ -0,0 +1,35 @@
+/* Write a program to print alternate elements of an array.
+
+Input: 10 20 30 40 50 60
+Output: 10 30 50
+
+Input: 5 8 12 15 18
+Output: 5 12 18
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void PrintAlternate(int Arr[], int iSize)
+{
+    int i = 0;
+
+    for(i = 0; i < iSize; i = i + 2)
+    {
+        printf("%d ", Arr[i]);
+    }
+}
+
+
+int main()
+{
+    int Arr[] = {10, 20, 30, 40, 50, 60};
+    int iLength = sizeof(Arr)/sizeof(Arr[0]);
+
+    printf("Alternate elements:\n");
+    PrintAlternate(Arr, iLength);
+
+    return 0;
+}
