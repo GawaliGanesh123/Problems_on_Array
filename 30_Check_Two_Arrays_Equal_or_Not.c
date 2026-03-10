0e7ce0b5c9c667188140abc9a0b76c13fb07349f
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-03-10 09:46:51 +0530

    Add files via upload

---
+++ 30_Check_Two_Arrays_Equal_or_Not.c
@@ -0,0 +1,63 @@
+/* Write a program to check whether two arrays are equal or not.
+
+Input:  Array 1: 10 20 30 40
+        Array 2: 10 20 30 40
+
+Output: Arrays are equal
+
+Input:  Array 1: 10 20 30 40
+        Array 2: 10 20 35 40
+
+Output: Arrays are not equal
+
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int CheckArrayEqual(int Arr1[], int Arr2[], int iSize1, int iSize2)
+{
+    int i = 0;
+
+    if(iSize1 != iSize2)
+    {
+        return 0;   // Not equal (different sizes)
+    }
+
+    for(i = 0; i < iSize1; i++)
+    {
+        if(Arr1[i] != Arr2[i])
+        {
+            return 0;   // Not equal
+        }
+    }
+
+    return 1;   // Equal
+}
+
+int main()
+{
+    int Arr1[] = {10, 20, 30, 40};
+    int Arr2[] = {10, 20, 30, 40};
+
+    int iSize1 = sizeof(Arr1)/sizeof(Arr1[0]);
+    int iSize2 = sizeof(Arr2)/sizeof(Arr2[0]);
+
+    int iRet = 0;
+
+    iRet = CheckArrayEqual(Arr1, Arr2, iSize1, iSize2);
+
+    if(iRet == 1)
+    {
+        printf("Arrays are equal\n");
+    }
+    else
+    {
+        printf("Arrays are not equal\n");
+    }
+
+    return 0;
+}
